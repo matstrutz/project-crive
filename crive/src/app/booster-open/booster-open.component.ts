f8c97d62aa2ef173573f53504d5f6e87e29fa7f8
matstrutz <tuffaogamer@hotmail.com>
2023-02-22 23:43:09 -0300

    feat: adicionando card css

--- crive/src/app/booster-open/booster-open.component.ts
+++ crive/src/app/booster-open/booster-open.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-booster-open',
   templateUrl: './booster-open.component.html',
-  styleUrls: ['./booster-open.component.scss']
+  styleUrls: ['./booster-open.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BoosterOpenComponent {
 
