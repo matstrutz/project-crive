37148d4569934b092a24e34c7e1015054b39047d
matstrutz <tuffaogamer@hotmail.com>
2023-02-23 20:05:49 -0300

    feat: centralizando carta

--- src/app/booster-open/booster-open.component.ts
+++ src/app/booster-open/booster-open.component.ts
@@ -4,7 +4,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-booster-open',
   templateUrl: './booster-open.component.html',
   styleUrls: ['./booster-open.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class BoosterOpenComponent {
 
